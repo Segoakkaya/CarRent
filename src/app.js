6d32c6647e1c0e3ae57d4e739a193b2fb2c46009
Segoakkaya <akkayasergen59@gmail.com>
2023-11-05 17:47:41 +0300

    index.html update

---
+++ src/app.js
@@ -0,0 +1,8 @@
+//require("../node_modules/jquery/dist/jquery")
+window.$ = $;
+window.jQuery = $;
+jQuery = $;
+    require("../node_modules/bootstrap/dist/js/bootstrap"),
+    require("../node_modules/daterangepicker/daterangepicker"),
+    require("../node_modules/owl.carousel/dist/owl.carousel.min"),
+    require("./js/script");

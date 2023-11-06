2f9007b160d4a5190105b9163070c149e458bfae
Segoakkaya <akkayasergen59@gmail.com>
2023-11-06 17:47:18 +0300

    CarDetail.html updateee

--- src/js/script.js
+++ src/js/script.js
@@ -249,6 +249,19 @@
                 clicked=false;
             }
             });
+            const sliderEl = document.querySelector("#range2")
+            const sliderValue = document.querySelector(".value2")
+            
+            sliderEl.addEventListener("input", (event) => {
+              const tempSliderValue = event.target.value; 
+              sliderValue.textContent = tempSliderValue;
+              
+              const progress = (tempSliderValue / sliderEl.max) * 100;
+             
+              sliderEl.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
+            })
+            
+
         },
         utility: {
             cookie: {

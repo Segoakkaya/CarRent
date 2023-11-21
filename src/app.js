75bf2fa903d222ff6655b3407f0c1218c367b599
Segoakkaya <akkayasergen59@gmail.com>
2023-11-21 17:29:09 +0300

    end

--- src/app.js
+++ src/app.js
@@ -5,4 +5,5 @@ jQuery = $;
     require("../node_modules/bootstrap/dist/js/bootstrap"),
     require("../node_modules/daterangepicker/daterangepicker"),
     require("../node_modules/owl.carousel/dist/owl.carousel.min"),
+    require("../node_modules/owl.carousel2.thumbs/dist/owl.carousel2.thumbs"),
     require("./js/script");
